7e61fcb89b3ca1a8ce7037ead69e03b7cbf540d8
Nefelie <nefelie.hemrich@gmail.com>
2023-05-13 16:25:29 +0100

    pollutant concentration stops code from working

--- MAChallengeV2/main.py
+++ MAChallengeV2/main.py
@@ -1,8 +1,9 @@
 """runs the main code"""
 # from boat_simulator import Simulator
 # from boat_simulator2 import local_Simulator
-from boat_simulator_trial import Simulator
+# from boat_simulator_trial import Simulator
 
+from boat_simulator_trial_pol import Simulator
 sim = Simulator('data.txt')
 #sim = local_Simulator('data.txt')
 #sim.simulate()
