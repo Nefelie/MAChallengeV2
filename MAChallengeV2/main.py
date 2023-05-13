97b5169b75d872b1e7aeb81c8bd85f87ad780141
Nefelie <nefelie.hemrich@gmail.com>
2023-05-13 15:35:09 +0100

    Initial commit

---
+++ MAChallengeV2/main.py
@@ -0,0 +1,26 @@
+"""runs the main code"""
+# from boat_simulator import Simulator
+# from boat_simulator2 import local_Simulator
+from boat_simulator_trial import Simulator
+
+sim = Simulator('data.txt')
+#sim = local_Simulator('data.txt')
+#sim.simulate()
+sim.simulate()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
